a56134c9ecf83aa8ae802e0b54baba0f3559bd42
EricCao20 <ericcao22@hotmail.com>
2023-03-09 22:21:06 -0500

    finished checkIsDead function

--- server/services/webscraper.js
+++ server/services/webscraper.js
@@ -1,8 +1,33 @@
+const fetch = require("node-fetch")
 
 const checkIsDead = async (recipeUrl) => {
 
     // Function to check if the HTML of the page is retrievable
     // returns true if link is alive, false if dead
+    try{
+        fetch(recipeUrl)
+        .then((rep) => {
+            if (rep.status === 404){
+                console.log("false")
+                return false
+            }
+            else{
+                console.log("true")
+                return true
+            }
+            //console.log(rep)
+        }
+
+        )
+    }
+    catch(err){
+        console.error(err)
+    }
+    
+
+
+
+
     console.log(recipeUrl);
 
     return true;
@@ -20,3 +45,5 @@ const addDirections = async (recipeUrl) => {
 }
 
 module.exports = { checkIsDead, addDirections }
+
+checkIsDead("https://www.seriouseats.com/recipes/2011/10/cook-the-book-carroty-mac-and-cheese.html")
