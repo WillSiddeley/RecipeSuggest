d5af8ba51db6093f9d131fa88df6d65db9fe5b8c
EricCao20 <ericcao22@hotmail.com>
2023-03-13 00:25:21 -0400

    added functions to webscraper.js

--- server/services/webscraper.js
+++ server/services/webscraper.js
@@ -1,6 +1,11 @@
 //const fetch = require("node-fetch")
+const fs = require("fs")
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom
+const recipeScraper = require("recipe-scraper")
+const jsonld = require("jsonld")
+
+const regex = /(?:instructions|method|directions|steps)[:\s]*([\s\S]+?)(?:\n\n|\r\n\r\n|$)/i;
 
 const checkLink = async (recipeUrl) => {
     // Function to check if the HTML of the page is retrievable
@@ -24,44 +29,54 @@ const checkLink = async (recipeUrl) => {
         console.error(error)
         return false
     })
-    
-    
 }
 
 const addDirections = async (recipeUrl) => {
     // Function to scrape the HTML of the page for recipe directions
     // returns string array with directions of the recipe step by step
-    try{
-        const response = await fetch(recipeUrl);
-        const html = await response.text();
-    
-        // Parse the HTML using a DOM parser
-        const parser = new JSDOM().window;
-        const doc = parser.parseFromString(html, 'text/html');
-    
-        // Find the recipe steps element(s) in the DOM
-        const recipeStepsElement = doc.querySelector('.directions ol');
-    
-        // Extract the individual recipe steps from the recipe steps element
-        const recipeSteps = Array.from(recipeStepsElement.children).map((stepElement) => {
-            return stepElement.textContent.trim();
-        })
-        console.log(recipeSteps)
-        return recipeSteps
-    }
-    catch(err){
-        console.error(err)
-    }
+    await recipeScraper(recipeUrl).then((recipe) => {
+        //recipe = JSON.parse(recipe)
+        console.log(recipe["instructions"])
+
+    })
 
 }
 
     //return ["step1", "step2", "step3"]
     //console.log(recipeUrl);
 
+const addDirectionRegex = async(recipeUrl) => {
 
+    return fetch(recipeUrl).then((res) =>{
+        return res.text()
 
-module.exports = { checkLink, addDirections }
+    }).then((html) => {
 
-checkLink("https://www.seriouseats.com/recipes/2011/10/cook-the-book-carroty-mac-and-cheese.html")
+        console.log(html)
 
-//addDirections("https://www.realsimple.com/food-recipes/browse-all-recipes/slow-roasted-lemon-and-herb-chicken")
+    }).catch((error) => {
+        console.error(error)
+    })
+
+    const match = ("view-source:" + recipeUrl).match(regex)
+
+    if (match){
+        const instructions = match[1].trim()
+        console.log(instructions)
+    }
+}
+
+const addDirectionLd = async(recipeUrl) => {
+
+        const expanded = await jsonld.expand(recipeUrl)
+        console.log(expanded)
+
+    }
+
+module.exports = { checkLink, addDirections, addDirectionRegex, addDirectionLd }
+
+//checkLink("https://www.seriouseats.com/recipes/2011/10/cook-the-book-carroty-mac-and-cheese.html")
+
+//addDirections("https://www.allrecipes.com/recipe/277953/air-fryer-beyond-meat-brats-onions-and-peppers/")
+
+addDirectionLd("https://www.marthastewart.com/326792/beef-ragu-with-pasta")
