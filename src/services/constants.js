f0988d77e42aa50401ab1e5dd6476fe717a42f4e
WillSiddeley <will.iams.sapo@gmail.com>
2023-03-13 22:48:37 -0400

    Change IP address to AWS

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,5 +1,5 @@
 
-//const baseAPI = "http://44.214.118.128:3001";
-const baseAPI = "http://192.168.2.222:3001";
+const baseAPI = "http://44.214.118.128:3001";
+//const baseAPI = "http://192.168.2.222:3001";
 
 module.exports = { baseAPI }
